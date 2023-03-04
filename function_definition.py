4c9730651b65719938c2dd9c5a0660cbaa964914
RiccardoMurgia <murgiariccardo96@gmail.com>
2023-03-04 23:18:37 +0100

    1 Changed the order of the nodes in order to draw them efficiently 2 Added discretize function 3 Renamed predict function to get_the_posterior_probability because it doesn't product a prediction for each datapoint but a posterior probability table. Ex: P(level | 'chest pain'=4, 'age'= 'child') +------------+--------------+ | level      |   phi(level) | +============+==============+ | level(0.0) |       0.3246 | +------------+--------------+ | level(1.0) |       0.4329 | +------------+--------------+ | level(2.0) |       0.2425 | +------------+--------------+ 4 Added the classify function

--- function_definition.py
+++ function_definition.py
@@ -1,15 +1,35 @@
-import matplotlib.pyplot as plt
-from pandas import DataFrame
 from pgmpy.inference import VariableElimination
 from pgmpy.models import NaiveBayes, BayesianNetwork
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import networkx as nx
-import math
 
 
-def get_node_size(label:'str'):
-    '''
-    returns the necessary dimensions of a circle for the lable to be inside the circle
-    '''
+def discretize(df, columns_to_discretize, bins_edges, categories=None):
+    """
+
+    :param df: pandas dataframe
+    :param columns_to_discretize: List of the features that you want to discretize
+    :param bins_edges: Dictionary containing for each feature the edges of the bins
+    :param categories: Dictionary containing for each feature the name of each bin
+    :return: A copy of the discretized dataframe
+    """
+
+    for col in columns_to_discretize:
+        df[col] = pd.cut(df[col], bins_edges[col], labels=categories[col])
+
+    return df
+
+
+def get_node_size(label: 'str'):
+    """
+
+    :param label: Name of the node.
+    :return: The necessary dimensions of a circle for the lable to be inside the circle
+    """
+
     fig, ax = plt.subplots(figsize=(3, 3))
     text = ax.text(0.5, 0.5, label, ha='center', va='center', fontsize=12)
     bbox = text.get_window_extent()
@@ -18,17 +38,23 @@ def get_node_size(label:'str'):
     return 250 + max(width, height) * 40
 
 
-def draw_net(nodes:'list[str]', net:'BayesianNetwork|NaiveBayes', style='std'):
-    '''
-    draw the network passed as imput creating a big-enough circle for every node so that the text is inside the circle 
-    '''
+def draw_net(nodes: 'list[str]', net: 'BayesianNetwork|NaiveBayes', style='full', target=None):
+    """
+
+    :param nodes: List of nodes sorted in drawing order
+    :param net: Bayesian network
+    :param style: Indicates the style used to draw our net in the best way. It could be 'full', 'trimmed' or 'circular'
+    :param target: In case style='circular' the node 'target' will be positioned in te center of the circle
+    :return: The function hasn't a return value. It displays the structure of the net
+    """
+
     graph = nx.DiGraph(net.edges())
     node_sizes = [get_node_size(label) for label in graph.nodes]
 
     plt.figure(figsize=(15, 10))
     pos = {}
 
-    if style == 'std':
+    if style == 'full':
         for i in range(len(graph.nodes)):
             if i < 2:
                 pos[nodes[i]] = [i, 4]
@@ -41,9 +67,22 @@ def draw_net(nodes:'list[str]', net:'BayesianNetwork|NaiveBayes', style='std'):
             else:
                 pos[nodes[i]] = [(i - 18), 0]
 
+    elif style == 'trimmed':
+        for i in range(len(graph.nodes)):
+            if i < 1:
+                pos[nodes[i]] = [i, 4]
+            elif i < 9:
+                pos[nodes[i]] = [(i - 5), 3]
+            elif i < 13:
+                pos[nodes[i]] = [(i - 10), 2]
+            elif i < 18:
+                pos[nodes[i]] = [(i - 15), 1]
+            else:
+                pos[nodes[i]] = [(i - 18), 0]
+
     else:
         pos = nx.circular_layout(graph, center=(0, 0))
-        pos['level'] = (-0.01, 0)
+        pos[target] = (-0.01, 0)
 
     nx.draw_networkx_nodes(graph, pos, node_color='lightblue', node_size=node_sizes)
     nx.draw_networkx_edges(graph, pos, width=1.5, alpha=0.5, arrows=True, arrowstyle='->,head_width=0.1,head_length=2')
@@ -52,18 +91,37 @@ def draw_net(nodes:'list[str]', net:'BayesianNetwork|NaiveBayes', style='std'):
     plt.show()
 
 
-def predict(model: 'VariableElimination', data: 'DataFrame', target: 'str'):
-    '''
-    given a Variable elimination model, a dataframe and a target, returns a list with all the prediction of the target for every data point in the data. 
-    '''
+def get_the_posterior_probability(model: 'VariableElimination', data: 'pd.DataFrame', target: 'str'):
+    """
+
+    :param model: VariableElimination obtained bya Bayesian Net
+    :param data: Dataframe containing the data
+    :param target: Variable to predictt
+    :return: A list of DiscreteFactor containing the posterior probability associated ad every datapoint
+    """
+
     result = []
     for _, datapoint in data.iterrows():
         row = datapoint.to_dict()
-        row_result = model.query(
+        posterior_probs = model.query(
             variables=[target],
             evidence=row,
             show_progress=False
         )
-        result.append(row_result)
-
+        result.append(posterior_probs)
     return result
+
+
+def classify(posterior_prob_tabs: 'lisr[DiscreteFactor]'):
+    """
+
+    :param posterior_prob_tabs: List of posterior probability tables
+    :return: A list of predicted labels. Each
+            datapoint are classified to the element with a maximum posterior probability
+    """
+
+    predicted_labels = []
+    for ppt in posterior_prob_tabs:
+        predicted_labels.append(ppt.state_names[ppt.variables[0]][np.argmax(ppt.values)])
+
+    return predicted_labels
