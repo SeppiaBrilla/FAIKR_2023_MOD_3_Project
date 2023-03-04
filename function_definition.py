565b3c1472d563c94c9a0810e3ecdbf6578cd9c8
RiccardoMurgia <murgiariccardo96@gmail.com>
2023-03-04 09:15:21 +0100

    modified drawnet

--- function_definition.py
+++ function_definition.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from pgmpy.inference import VariableElimination
 from pgmpy.models import NaiveBayes, BayesianNetwork
 import networkx as nx
+import math
 
 
 def get_node_size(label:'str'):
@@ -17,31 +18,36 @@ def get_node_size(label:'str'):
     return 250 + max(width, height) * 40
 
 
-def draw_net(nodes:'list[str]', net:'BayesianNetwork|NaiveBayes'):
+def draw_net(nodes:'list[str]', net:'BayesianNetwork|NaiveBayes', style='std'):
     '''
     draw the network passed as imput creating a big-enough circle for every node so that the text is inside the circle 
     '''
     graph = nx.DiGraph(net.edges())
     node_sizes = [get_node_size(label) for label in graph.nodes]
 
+    plt.figure(figsize=(15, 10))
     pos = {}
-    for i in range(len(graph.nodes)):
-        if i < 2:
-            pos[nodes[i]] = [i, 4]
-        elif i < 10:
-            pos[nodes[i]] = [(i - 5), 3]
-        elif i < 14:
-            pos[nodes[i]] = [(i - 10), 2]
-        elif i < 21:
-            pos[nodes[i]] = [(i - 15), 1]
-        else:
-            pos[nodes[i]] = [(i - 18), 0]
 
-    plt.figure(figsize=(15,10))
+    if style == 'std':
+        for i in range(len(graph.nodes)):
+            if i < 2:
+                pos[nodes[i]] = [i, 4]
+            elif i < 10:
+                pos[nodes[i]] = [(i - 5), 3]
+            elif i < 14:
+                pos[nodes[i]] = [(i - 10), 2]
+            elif i < 21:
+                pos[nodes[i]] = [(i - 15), 1]
+            else:
+                pos[nodes[i]] = [(i - 18), 0]
+
+    else:
+        pos = nx.circular_layout(graph, center=(0, 0))
+        pos['level'] = (-0.01, 0)
+
     nx.draw_networkx_nodes(graph, pos, node_color='lightblue', node_size=node_sizes)
     nx.draw_networkx_edges(graph, pos, width=1.5, alpha=0.5, arrows=True, arrowstyle='->,head_width=0.1,head_length=2')
     nx.draw_networkx_labels(graph, pos, font_color='black', font_size=10)
-
 
     plt.show()
 
